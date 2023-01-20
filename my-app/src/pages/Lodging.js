d382ecb6e2cc5b05a67a62697a0d32207bc15afc
matteo_d <matteodrouin33@gmail.com>
2023-01-20 22:47:24 +0100

    save : print id of URL param

--- my-app/src/pages/Lodging.js
+++ my-app/src/pages/Lodging.js
@@ -1,8 +1,10 @@
+import { useParams } from "react-router-dom"
 
 export function Lodging() {
+  const { id } = useParams()
   return(
     <div>
-      <h1>Lodging</h1>
+      <h1> Lodging { id } </h1>
     </div>
   )
 }
