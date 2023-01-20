3125ca07a7e97bd79f83cbc00bc6fd1d438bd461
matteo_d <matteodrouin33@gmail.com>
2023-01-20 23:16:19 +0100

    save : display info from data.json if id URL params match id in data.json

--- my-app/src/pages/Lodging.js
+++ my-app/src/pages/Lodging.js
@@ -1,10 +1,14 @@
 import { useParams } from "react-router-dom"
+import jsonData from "../data.json"
 
 export function Lodging() {
   const { id } = useParams()
+  const Data = jsonData.filter(prop => prop.id === id);
+  console.log(Data[0].title)
   return(
     <div>
-      <h1> Lodging { id } </h1>
+      <h1> URL PARAMS :  { id } </h1>
+      <p> {Data[0].title} </p>
     </div>
   )
 }
