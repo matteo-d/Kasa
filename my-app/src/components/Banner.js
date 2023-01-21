24838c8a10251da39db7f635c8a2ef0137196e4d
matteo_d <matteodrouin33@gmail.com>
2023-01-22 00:16:08 +0100

    save : i hate css

--- my-app/src/components/Banner.js
+++ my-app/src/components/Banner.js
@@ -2,27 +2,21 @@ import styled from "styled-components"
 
 const Section = styled.section`
 position: absolute;
-width: 100%;
-height: 223px;
-top: 171px;
+display : flex;
+width : 100vw;
+justify-content: center;
 background-color : red
 `
 const Title = styled.h1`
   position: absolute;
-left: 25.9%;
-right: 25.14%;
-top: 24.32%;
-bottom: 69.04%;
 
 font-family: 'Montserrat';
 font-style: normal;
 font-weight: 500;
 font-size: 48px;
-line-height: 142.6%;
 /* or 68px */
 
-display: flex;
-align-items: flex-end;
+
 
 color: black ;
 `
