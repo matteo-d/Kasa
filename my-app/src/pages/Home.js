526fb53c88afd3cbe05a126a01a63daf9edd09cd
matteo_d <matteodrouin33@gmail.com>
2023-01-21 18:46:50 +0100

    save : using fragments to display multiple components in a page + display for each lodgment the title and cover using map/key

--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -1,8 +1,12 @@
+import Banner from "../components/Banner"
+import Card from "../components/Card"
 
 export function Home() {
   return(
-    <div>
-      <h1>Home</h1>
-    </div>
+    // Bellow using new react frangments syntax ( the empty <>)
+    <>
+    <Banner />
+    <Card />
+    </>
   )
 }
