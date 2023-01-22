9205347c949639954e4eeabb7cadb31c46e4ea54
matteo_d <matteodrouin33@gmail.com>
2023-01-22 02:40:27 +0100

    save : stuck Header css

--- my-app/src/components/Banner.js
+++ my-app/src/components/Banner.js
@@ -1,32 +1,9 @@
 import styled from "styled-components"
 
-const Section = styled.section`
-position: absolute;
-display : flex;
-width : 100vw;
-justify-content: center;
-background-color : red
-`
-const Title = styled.h1`
-  position: absolute;
-
-font-family: 'Montserrat';
-font-style: normal;
-font-weight: 500;
-font-size: 48px;
-/* or 68px */
-
-
-
-color: black ;
-`
-
-
-/* IMG */
 export default function Banner(){
   return(
-    <Section>
-      <Title>Chez vous, partout et ailleurs</Title>
-    </Section>
+    <section>
+      <title>Chez vous, partout et ailleurs</title>
+    </section>
   )
 }
--- my-app/src/components/Card.js
+++ my-app/src/components/Card.js
@@ -1,35 +1,15 @@
 import jsonData from "../data.json"
-import styled from "styled-components"
-
-const Section = styled.section`
- position: absolute;
-    width: 100%;
-   
-    
-    top: 30rem;
-    background-color: red;
-} 
-`
-const Ul = styled.ul`
   
-
-`
-const Li = styled.li`
-
-`
-const Title = styled.h3`
-
-`
 export default function Card(){ 
   return(
-    <Section>
-      <Ul>
+    <section>
+      <ul>
         {jsonData.map(lodgement => (
-          <Li key={lodgement.id}>
-            <Title>{lodgement.title}</Title>
-          </Li>
+          <li key={lodgement.id}>
+            <title>{lodgement.title}</title>
+          </li>
         ))}
-      </Ul>
-    </Section>
+      </ul>
+    </section>
   )
 }
