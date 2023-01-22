5f6e825fd350ce9c5d320ad455b5d630e2053e41
matteo_d <matteodrouin33@gmail.com>
2023-01-22 08:55:37 +0100

    save fix svg logo

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -1,11 +1,11 @@
 import { Link } from "react-router-dom"
 import { HeaderStyled, Img, Nav, Home, About } from "../styles/Header"
+import Svg from "../assets/logo.svg"
 
 export default function Header(){
   return(
     <HeaderStyled>
-      <Img  src="./kasa_logo.PNG"
-     alt="Logo de Kasa"/>
+      <Img  src={ Svg } alt="Logo de Kasa"/>
       <Nav>
         <Home to="/"> Home </Home>
       <About to="/about"> About </About>
