def75f11d6a4e9980e902ba7489ac326a39f5447
matteo_d <matteodrouin33@gmail.com>
2023-01-23 10:04:43 +0100

    meh

--- my-app/src/components/Collapse.js
+++ my-app/src/components/Collapse.js
@@ -5,11 +5,11 @@ import ArrowDown from "../assets/arrowDown.svg"
 import ArrowUp from "../assets/arrowUp.svg"
   // Déclare variable d'état
 export default function Collapse() {
-  const [text, setText] = useState("none")
-  const [arrow, setArrow] = useState("url(${ArrowDown})")
+  const [display, setDisplay] = useState("none")
+  const [bgImage, setBgImage] = useState(`url(${ArrowDown})`)
 function toggleArrow(){
-  setArrow(arrow === "url(${ArrowDown})" ? "url(${ArrowUp})" : "url(${ArrowDown})")
-  setText(text === "none" ? "block" : "none")
+  setBgImage( bgImage=== "url(${ArrowDown})" ? "url(${ArrowUp})" : "url(${ArrowDown})")
+  setDisplay(display === "none" ? "block" : "none")
 }
   return(
   <Section>
